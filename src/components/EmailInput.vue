9ed39e1b50da79d048e5060fad74e8f1e178be79
Ali Elafoury <alielasfoury12@gmail.com>
2025-11-17 11:27:58 +0200

    create login and register pages

---
+++ src/components/EmailInput.vue
@@ -0,0 +1,14 @@
+<script setup>
+    const {form, errors} = defineProps({
+        form: Object,
+        errors: Object
+    })    
+</script>
+
+<template>
+    <div class="field">
+        <label>Email</label>
+        <input type="email" v-model="form.email" />
+        <small v-if="errors.email">{{ errors.email }}</small>
+    </div>
+</template>
---
+++ src/components/PasswordInput.vue
@@ -0,0 +1,14 @@
+<script setup>
+    const {form, errors} = defineProps({
+        form: Object,
+        errors: Object
+    })    
+</script>
+
+<template>
+    <div class="field">
+        <label>Password</label>
+        <input type="password" v-model="form.password" />
+        <small v-if="errors.password">{{ errors.password }}</small>
+    </div>
+</template>
